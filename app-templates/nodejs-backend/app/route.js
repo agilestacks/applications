ade45b648b1018f33a84d4ca97b95ace5d2ece7a
Igor Lysak <igorlysak@agilestacks.com>
2018-10-31 18:13:06 +0200

    Nodejs-backend application converted

--- app-templates/nodejs-backend/app/route.js
+++ app-templates/nodejs-backend/app/route.js
@@ -1,5 +1,5 @@
 const statusController = require('./controllers/status_controller');
 
 module.exports = (app) => {
-  app.get('/status', statusController.getStatus);
+  app.get('/', statusController.getStatus);
 };
