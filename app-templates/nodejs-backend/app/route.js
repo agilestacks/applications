020880de243c1171effa5b4ef97d7c8ddd3c08ee
Igor Lysak <igorlysak@agilestacks.com>
2018-11-01 17:27:50 +0200

    nodejs-backend: add app route for /status path

--- app-templates/nodejs-backend/app/route.js
+++ app-templates/nodejs-backend/app/route.js
@@ -2,4 +2,5 @@ const statusController = require('./controllers/status_controller');
 
 module.exports = (app) => {
   app.get('/', statusController.getStatus);
+  app.get('/status', statusController.getStatus);
 };
