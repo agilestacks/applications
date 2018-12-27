00116712d40a0084bc4c556cbdd4d2db09d96412
Igor Lysak <igorlysak@agilestacks.com>
2018-12-27 11:50:50 +0200

    Feature toggle for nodejs-backend app

--- app-templates/nodejs-backend/app/controllers/status_controller.js
+++ app-templates/nodejs-backend/app/controllers/status_controller.js
@@ -2,10 +2,17 @@ const pkg = require('../../package');
 
 module.exports = {
   getStatus(req, res) {
-    res.json({
+    const body = {
       name: pkg.name,
       version: pkg.version,
       uptime: process.uptime(),
-    });
+    };
+    if (process.env.ENVIRONMENT_NAME) {
+      res.json(Object.assign({}, body, {
+        environment: process.env.ENVIRONMENT_NAME,
+      }));
+    } else {
+      res.json(body);
+    }
   },
 };
