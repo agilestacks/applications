fbd0f7eb0fcd356a3bd1ad04e24f2aedd9ed6926
Antons Kranga <anton@agilestacks.com>
2019-01-22 20:11:25 +0200

    Restructured magicts

--- app-templates/kubeflow-pipeline/environment.py
+++
@@ -1,5 +0,0 @@
-from ipython_secrets import *
-
-set_secret('S3_ENDPOINT', 'https://minio.app.cluster3.antoncloud1.dev.superhub.io')
-set_secret('S3_ACCESS_KEY', 'minio')
-set_secret('S3_SECRET_KEY', 'minio1234')
