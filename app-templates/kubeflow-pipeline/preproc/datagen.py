ea3a604cc2f5d0193ff8c004394238e6b161856d
Antons Kranga <anton@agilestacks.com>
2019-02-07 01:47:00 +0200

    aws support for data prep

--- app-templates/kubeflow-pipeline/preproc/datagen.py
+++ app-templates/kubeflow-pipeline/preproc/datagen.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright 2018 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,54 +21,111 @@ import glob
 import os
 import re
 import subprocess
-from urlparse import urlparse
 
-import googleapiclient.discovery
-from google.cloud import storage
+from tempfile import NamedTemporaryFile
 
+import requests
+
+try: 
+  from urlparse import urlparse 
+except ImportError: 
+  from urllib.parse import urlparse
+
+
+def http_download(url, local_filename):
+  r = requests.get(url, stream=True)
+  with open(local_filename, 'wb') as f:
+    for chunk in r.iter_content(chunk_size=8192): 
+      if chunk: # filter out keep-alive new chunks
+        f.write(chunk)
+
+
+def gcs_download(url, local_filename):
+  cmd = ['gsutil', 'cp', url, local_filename]
+  result = subprocess.call(data_copy_command1)
+  print(result)
+
+
+def s3_download(url, local_filename, s3_client=None):
+  import boto3
+  if not s3_client:
+    s3_client = boto3.client('s3')
+
+  o = urlparse( package )
+  bucket = o.netloc
+  key = o.path.lstrip('/')
+  with open(local_filename, 'wb') as data:
+    s3.download_fileobj(bucket, key, data)
 
 def copy_local_directory_to_gcs(project, local_path, bucket_name, gcs_path):
-    """Recursively copy a directory of files to GCS.
+  """Recursively copy a directory of files to GCS.
 
-    local_path should be a directory and not have a trailing slash.
-    """
-    assert os.path.isdir(local_path)
-    for local_file in glob.glob(local_path + '/**'):
-        if not os.path.isfile(local_file):
-            continue
-        remote_path = os.path.join(gcs_path, local_file[1 + len(local_path) :])
-        storage_client = storage.Client(project=project)
-        bucket = storage_client.get_bucket(bucket_name)
-        blob = bucket.blob(remote_path)
-        blob.upload_from_filename(local_file)
+  local_path should be a directory and not have a trailing slash.
+  """
+
+  import googleapiclient.discovery
+  from google.cloud import storage
+
+  assert os.path.isdir(local_path)
+  for local_file in glob.glob(local_path + '/**'):
+    if not os.path.isfile(local_file):
+        continue
+    remote_path = os.path.join(gcs_path, local_file[1 + len(local_path) :])
+    storage_client = storage.Client(project=project)
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(remote_path)
+    blob.upload_from_filename(local_file)
+
+def copy_local_directory_to_s3(local_path, bucket_name, s3_path, s3_client=None):
+  import boto3
+  if not s3_client:
+    s3_client = boto3.client('s3')
+  for root,dirs,files in os.walk(path):
+    for file in files:
+      path_from = os.path.join(root,file)
+      path_to = os.path.join(s3_path,file)
+      s3_client.upload_file(path_from, bucketname, path_to)
+
 
 def main(argv=None):
   parser = argparse.ArgumentParser(description='ML Trainer')
   parser.add_argument(
-      '--data-dir',
+      '--data-source-file',
+      help='...',
+      required=True)
+  parser.add_argument(
+      '--result-remote-dir',
       help='...',
       required=True)
   parser.add_argument(
       '--project',
       help='...',
-      required=True)
+      required=False)
 
   args = parser.parse_args()
 
   problem = 'gh_problem'
-  remote_data_dir = args.data_dir
-  local_data_dir = '/ml/t2t_gh_data'
-  local_source_data_file = '/ml/gh_data/github_issues.csv'
+  remote_data_file = args.data_source_file
+  remote_data_dir = args.result_remote_dir
+  local_data_dir = '/ml/t2t_gh_data/'
+  local_data_file = '/ml/gh_data/github_issues.csv'
 
-  data_copy_command1 = ['gsutil', 'cp', 'gs://aju-dev-demos-codelabs/kubecon/gh_data/github_issues.csv', local_source_data_file
-      ]
-  print(data_copy_command1)
-  result = subprocess.call(data_copy_command1)
-  print(result)
+  # with NamedTemporaryFile(prefix=local_data_dir, suffix='.csv') as tmpfile:
+  print("Downloading: %s to %s" % (remote_data_file, local_data_file))
+  o = urlparse(remote_data_dir)
+  if o.scheme in ['http', 'https']:
+    http_download(remote_data_file, local_data_file)
+  elif o.scheme == 'gs':
+    gcs_download(remote_data_file, local_data_file)
+  elif o.scheme == 's3':
+    s3_download(remote_data_file, local_data_file)
+  else:
+    raise ValueError('Unsupported scheme: %s' % o.scheme)
+
+  print("Done!")
 
   datagen_command = ['t2t-datagen', '--data_dir', local_data_dir, '--t2t_usr_dir', '/ml/ghsumm/trainer', '--problem', problem,
-     '--tmp_dir', local_data_dir + '/tmp'
-     ]
+     '--tmp_dir', local_data_dir + '/tmp']
   print(datagen_command)
   result1 = subprocess.call(datagen_command)
   print(result1)
@@ -75,7 +134,12 @@ def main(argv=None):
   o = urlparse(remote_data_dir)
   path = re.sub('^/', '', o.path)
   print("using bucket: %s and path: %s" % (o.netloc, path))
-  copy_local_directory_to_gcs(args.project, local_data_dir, o.netloc, path)
+  if o.scheme == 'gcs':
+    copy_local_directory_to_gcs(args.project, local_data_dir, o.netloc, path)
+  elif o.scheme == 's3':
+    copy_local_directory_to_s3(args.project, local_data_dir, o.netloc, path)
+  else:
+    raise ValueError('Unsupported scheme: %s' % o.scheme)
 
 
 if __name__== "__main__":
