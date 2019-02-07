6639e03854da61d3374687f1b2d0380419c239c9
Victor Oginskis <victor@agilestacks.com>
2019-02-07 17:53:43 +0200

    revert

--- phase0/init.js
+++ phase0/init.js
@@ -194,6 +194,10 @@ async function perform() {
             if (parametersURL) {
                 const parameters = download(parametersURL);
                 copyParameters(parameters);
+                shelljs.cd(workspaceDir);
+                shelljs.exec(`git add .`);
+                shelljs.exec(`git commit -m "Parameters updated"`);
+                shelljs.exec(`git push`);
             }
             logger.info('Application workspace initialized');
         } else {
