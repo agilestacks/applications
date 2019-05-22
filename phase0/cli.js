ec4a69838f955c7f5257e24d8580f91b8c3cc2aa
Antons Kranga <anton@agilestacks.com>
2019-05-22 10:51:32 +0300

    cli.js introdcued context path (#101) and kubeflow payload example

--- phase0/cli.js
+++ phase0/cli.js
@@ -12,16 +12,18 @@ const path = require('path');
 var autoHubSrc = process.env.AUTOMATION_HUB_SRC || '../../automation-hub/api/src/'
 const app = require(path.join(autoHubSrc, 'controllers/applications'))
 
-var applicationTemplates = process.env.APPLICATION_TEMPLATES || '../app-templates/'
+// var applicationTemplates = process.env.APPLICATION_TEMPLATES || '../app-templates/'
 
 const cli = meow(`
     Usage
-    $ cli.js <input>
+    $ cli.js <flags> <app-root>
+
+    app-root: directory where application source code is (<app-root>/.hub)
 
     Options
-    --file, -f use manifest to generate another
-    --output -o save manifest into file
-
+    --file, -f request payload in yaml format
+    --output, -o save manifest into file (if empty then to stdout)
+    --requires, -r comma separated list of slices (if empty will be taken from payload)
 `, {
         flags: {
             file: {
@@ -31,6 +33,10 @@ const cli = meow(`
             output: {
                 type: 'string',
                 alias: 'o'
+            },
+            requires: {
+                type: 'string',
+                alias: 'r'
             },
         }
     });
@@ -56,13 +62,19 @@ function writeFile(file, content) {
 }
 
 if (module.parent == null) {
-    const inputFile = cli.flags.file
-    const outputFile = cli.flags.output
+    var inputFile = cli.flags.file
+    var outputFile = cli.flags.output
+    var appRootDir = cli.input[0] || '.'
 
     var doc = readFile(inputFile)
-    var requires = ['hub', ...doc.requires]
+    var requires
+    if (cli.flags.requires) {
+        requires = ['hub', ...cli.flags.requires.split(',')]
+    } else {
+        requires = ['hub', ...doc.requires]
+    }
     self.logger.info('App requires: [' + requires + ']')
-    var slicesDir = path.join(applicationTemplates, doc.application, '.hub/manifests')
+    var slicesDir = path.join(appRootDir, '.hub/manifests')
     var slices = fs.readdirSync(slicesDir).filter(file =>
         requires.includes(app.extractSliceComponentName(file) )
     ).map(
