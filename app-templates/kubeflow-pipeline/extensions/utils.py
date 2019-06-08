34dd283d8ea769c21eb2712e4e2637272189673e
Antons Kranga <anton@agilestacks.com>
2019-06-08 20:04:08 +0300

    Kubeflow: download_file has been moved into utilities

--- app-templates/kubeflow-pipeline/extensions/utils.py
+++ app-templates/kubeflow-pipeline/extensions/utils.py
@@ -1,6 +1,6 @@
 from kubernetes import config
 import hashlib
-
+import os.path
 
 def sha1(*argv):
     """returns sha1 encoded string. optionally supports salt
@@ -22,3 +22,31 @@ def current_namespace():
         return open('/var/run/secrets/kubernetes.io/serviceaccount/namespace').read()
     except OSError:
         return 'default'
+
+
+def md5sum(filename):
+    """Returns md5 sum"""
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def download_file(url, download_to, md5sum):
+    """Downloads file from remote url"""
+    # NOTE the stream=True parameter below
+
+    if os.path.isfile(download_to) and md5sum:
+        if md5sum == md5sum(download_to):
+            print(f"It seems {download_to} has been already downloaded")
+            print("Moving on...")
+            return
+
+    print('Downloading...')
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(download_to, 'wb+') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+    print('Done!')
