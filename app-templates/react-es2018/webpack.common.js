17f6d26593a32fded3dbd0e84e9abedca70b4d8b
Igor Lysak <igorlysak@agilestacks.com>
2019-08-05 15:17:34 +0300

    react-app: update dependencies

--- app-templates/react-es2018/webpack.common.js
+++ app-templates/react-es2018/webpack.common.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
 
@@ -95,7 +95,7 @@ module.exports = {
             ]
         },
         plugins: [
-            new CleanWebpackPlugin(['dist']),
+            new CleanWebpackPlugin(),
             new MiniCssExtractPlugin({
                 filename: devMode ? '[name].css' : '[name].[hash].css',
                 chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
