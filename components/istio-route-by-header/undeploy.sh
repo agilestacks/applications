cbb4869f847122496d66d3fbc0f8e1e69a650050
Victor Oginskis <victor@agilestacks.com>
2019-09-20 15:34:09 +0300

    typo

--- components/istio-route-by-header/undeploy.sh
+++ components/istio-route-by-header/undeploy.sh
@@ -2,5 +2,5 @@
 
 kubectl="kubectl --context=$DOMAIN_NAME --namespace=$NAMESPACE"
 
-$kubectl delete $SERVICE_NAME-virtual
-$kubectl delete $SERVICE_NAME-rule
+$kubectl delete virtualservice $SERVICE_NAME-virtual
+$kubectl delete destinationrule $SERVICE_NAME-rule
