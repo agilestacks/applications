fb7fd849d986d6c8ca724c9a832e1a787f1ce24b
Victor Oginskis <victor@agilestacks.com>
2019-09-20 18:17:25 +0300

    istio sidecar uninject on application undeployment

--- components/istio-route-by-header/undeploy.sh
+++ components/istio-route-by-header/undeploy.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+SERVICE_LABEL_NAME=app
+
 kubectl="kubectl --context=$DOMAIN_NAME --namespace=$NAMESPACE"
+istioctl="istioctl --context=$DOMAIN_NAME"
 
 $kubectl delete virtualservice $SERVICE_NAME-virtual
 $kubectl delete destinationrule $SERVICE_NAME-rule
+
+export DEPLOYMENTS="`$kubectl get deployments --selector=$SERVICE_LABEL_NAME=$SERVICE_NAME \
+  -o json | jq -rM '.items[].metadata.name' | xargs`"
+
+for DEPLOYMENT in "$DEPLOYMENTS"
+do
+  $kubectl get deployment $DEPLOYMENT -o yaml | $istioctl x kube-uninject -f - | $kubectl apply -f -
+done
