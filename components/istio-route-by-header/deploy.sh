d3d916e9cd2ee966880a7319548e622cab5b31a5
Victor Oginskis <victor@agilestacks.com>
2019-09-20 15:30:41 +0300

    proper uninstall of istio configuration app

--- components/istio-route-by-header/deploy.sh
+++ components/istio-route-by-header/deploy.sh
@@ -22,7 +22,7 @@ if [ -z "$VERSIONS" ]; then
   exit 1
 fi
 
-for DEPLOYMENT in $DEPLOYMENTS
+for DEPLOYMENT in "$DEPLOYMENTS"
 do
   $kubectl get deployment $DEPLOYMENT -o yaml | $istioctl kube-inject -f - | $kubectl apply -f -
 done
--- components/istio-route-by-header/undeploy.sh
+++ components/istio-route-by-header/undeploy.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-jsonnet destination-rule.jsonnet \
-  --ext-str SERVICE_NAME \
-  --ext-str VERSIONS -y > destination-rule.yaml
-jsonnet virtual-service-route-by-header.jsonnet \
-  --ext-str SERVICE_NAME \
-  --ext-str DEFAULT_VERSION \
-  --ext-str INGRESS_GATEWAY_FQDN \
-  --ext-str GATEWAY_NAME \
-  --ext-str HEADER_NAME \
-  --ext-str VERSIONS -y > virtual-service-route-by-header.yaml
+kubectl="kubectl --context=$DOMAIN_NAME --namespace=$NAMESPACE"
 
-kubectl --context=$DOMAIN_NAME delete -f virtual-service-route-by-header.yaml
-kubectl --context=$DOMAIN_NAME delete -f destination-rule.yaml || true
+$kubectl delete $SERVICE_NAME-virtual
+$kubectl delete $SERVICE_NAME-rule
