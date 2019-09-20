8a56ec5ab8f9aaf7cf3bd1e6a33de7995a8e8ed2
Victor Oginskis <victor@agilestacks.com>
2019-09-20 13:30:20 +0300

    generate template on uninstall

--- components/istio-route-by-header/undeploy.sh
+++ components/istio-route-by-header/undeploy.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+jsonnet destination-rule.jsonnet \
+  --ext-str SERVICE_NAME \
+  --ext-str VERSIONS -y > destination-rule.yaml
+jsonnet virtual-service-route-by-header.jsonnet \
+  --ext-str SERVICE_NAME \
+  --ext-str DEFAULT_VERSION \
+  --ext-str INGRESS_GATEWAY_FQDN \
+  --ext-str GATEWAY_NAME \
+  --ext-str HEADER_NAME \
+  --ext-str VERSIONS -y > virtual-service-route-by-header.yaml
+
 kubectl --context=$DOMAIN_NAME delete -f virtual-service-route-by-header.yaml
 kubectl --context=$DOMAIN_NAME delete -f destination-rule.yaml || true
