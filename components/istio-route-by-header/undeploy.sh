8289915e97fde75e0c802d5a73a21b53c7ef00ba
Victor Oginskis <victor@agilestacks.com>
2019-09-24 16:28:04 +0300

    Istio configuration: uninstall indepotence

--- components/istio-route-by-header/undeploy.sh
+++ components/istio-route-by-header/undeploy.sh
@@ -5,13 +5,13 @@ SERVICE_LABEL_NAME=app
 kubectl="kubectl --context=$DOMAIN_NAME --namespace=$NAMESPACE"
 istioctl="istioctl --context=$DOMAIN_NAME"
 
-$kubectl delete virtualservice $SERVICE_NAME-virtual
-$kubectl delete destinationrule $SERVICE_NAME-rule
+$kubectl delete virtualservice $SERVICE_NAME-virtual || true
+$kubectl delete destinationrule $SERVICE_NAME-rule || true
 
 export DEPLOYMENTS="`$kubectl get deployments --selector=$SERVICE_LABEL_NAME=$SERVICE_NAME \
   -o json | jq -rM '.items[].metadata.name' | xargs`"
 
 for DEPLOYMENT in "$DEPLOYMENTS"
 do
-  $kubectl get deployment $DEPLOYMENT -o yaml | $istioctl x kube-uninject -f - | $kubectl apply -f -
+  $kubectl get deployment $DEPLOYMENT -o yaml | $istioctl x kube-uninject -f - | $kubectl apply -f - || true
 done
