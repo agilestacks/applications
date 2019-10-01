36e93c34e93e7be6505f95b70e069589472e4703
mameshini <igor@agilestacks.com>
2019-09-30 21:52:27 -0700

    Updated for file streaming

--- app-templates/python-video/stream-video-browser/webstreaming.py
+++ app-templates/python-video/stream-video-browser/webstreaming.py
@@ -26,6 +26,10 @@ app = Flask(__name__)
 # initialize the video stream and allow the camera sensor to
 # warmup
 #vs = VideoStream(usePiCamera=1).start()
+
+#vs = cv2.VideoCapture("video/jurassic_park_intro.mp4")
+#vs = FileVideoStream("video/jurassic_park_intro.mp4").start()
+
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
