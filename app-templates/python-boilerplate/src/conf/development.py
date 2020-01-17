eb37117a5d14d0b8f4e54caaca8f4ccc191a7bb0
Antons Kranga <anton@agilestacks.com>
2020-01-17 12:32:06 +0200

    Fixed misunderstanding with python.py execution

--- app-templates/python-boilerplate/src/conf/development.py
+++ app-templates/python-boilerplate/src/conf/development.py
@@ -2,3 +2,5 @@ from os import getenv
 DEBUG                  = True
 JSON_ADD_STATUS        = True
 JSON_STATUS_FIELD_NAME = 'code'
+HOST                   = 'localhost'
+PORT                   = 5000
--- app-templates/python-boilerplate/src/conf/production.py
+++ app-templates/python-boilerplate/src/conf/production.py
@@ -2,3 +2,5 @@ import os
 DEBUG                  = True
 JSON_ADD_STATUS        = True
 JSON_STATUS_FIELD_NAME = 'code'
+HOST                   = 'localhost'
+PORT                   = 5000
