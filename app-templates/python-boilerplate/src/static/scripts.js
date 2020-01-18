a4427b0f398b8c8eb94f6b392d1f8bdaa3ff7860
Artem Kudla <artem@agilestacks.com>
2020-01-18 03:39:27 +0200

    python-boilerplate: themming

--- app-templates/python-boilerplate/src/static/scripts.js
+++ app-templates/python-boilerplate/src/static/scripts.js
@@ -2,12 +2,19 @@ const LOADING_KEYWORD_CLASS = 'asi-teaser-keyword-loading';
 const FLASH_KEYWORD_CLASS = 'asi-teaser-keyword-flash';
 const TILE_SELECTOR = '.asi-tile';
 const KEYWORD_SELECTOR = '.asi-teaser-keyword';
+const THEME_TOGGLE_SELECTOR = '.asi-theme-toggle';
+const APP_SELECTOR = '.App';
 
 const KEYWORD_COLOR_CLASSES = [
-    'asi-teaser-keyword-orange',
-    'asi-teaser-keyword-red',
-    'asi-teaser-keyword-blue',
-    'asi-teaser-keyword-green'
+  'asi-teaser-keyword-orange',
+  'asi-teaser-keyword-red',
+  'asi-teaser-keyword-blue',
+  'asi-teaser-keyword-green'
+];
+const THEME_CLASSES = [
+  'asi-theme-default',
+  'asi-theme-dark',
+  'asi-theme-light'
 ];
 
 const KEYWORDS_URL = '/gimme/1';
@@ -42,6 +49,9 @@ function setNodeText(node, text) {
     node[prop] = text;
 }
 
+const localStorageGet = (name) => window.localStorage &&  window.localStorage.getItem(name);
+const localStorageSet = (name, value) => window.localStorage &&  window.localStorage.setItem(name, value);
+
 const keywordsHistory = [...document.querySelectorAll(KEYWORD_SELECTOR)]
     .map(getNodeText);
 
@@ -57,32 +67,32 @@ async function nextWord() {
     }
 }
 
-function addKeywordClassNames(keywordNode, ...classNames) {
+function addNodeClassNames(keywordNode, ...classNames) {
     keywordNode.classList.add(...classNames);
 }
 
-function removeKeywordClassNames(keywordNode, ...classNames) {
+function removeNodeClassNames(keywordNode, ...classNames) {
     keywordNode.classList.remove(...classNames);
 }
 
 async function updateKeyword(keywordNode) {
-    // switch keyword color
-    removeKeywordClassNames(keywordNode, ...KEYWORD_COLOR_CLASSES);
-    addKeywordClassNames(keywordNode, sample(KEYWORD_COLOR_CLASSES));
-
     // trigger keyword is loading
-    addKeywordClassNames(keywordNode, LOADING_KEYWORD_CLASS);
+    addNodeClassNames(keywordNode, LOADING_KEYWORD_CLASS);
 
     const word = await nextWord();
     setNodeText(keywordNode, word);
 
     // stop keyword is loading
-    removeKeywordClassNames(keywordNode, LOADING_KEYWORD_CLASS);
+    removeNodeClassNames(keywordNode, LOADING_KEYWORD_CLASS);
+
+    // switch keyword color
+    removeNodeClassNames(keywordNode, ...KEYWORD_COLOR_CLASSES);
+    addNodeClassNames(keywordNode, sample(KEYWORD_COLOR_CLASSES));
 
     // make keyword flash
-    addKeywordClassNames(keywordNode, FLASH_KEYWORD_CLASS);
+    addNodeClassNames(keywordNode, FLASH_KEYWORD_CLASS);
     setTimeout(() => {
-        removeKeywordClassNames(keywordNode, FLASH_KEYWORD_CLASS);
+        removeNodeClassNames(keywordNode, FLASH_KEYWORD_CLASS);
     }, 100);
 };
 
@@ -98,8 +108,39 @@ function updateKeywords() {
     }
 }
 
+function setTheme(theme) {
+  const appNode = document.querySelector(APP_SELECTOR);
+
+  removeNodeClassNames(appNode, ...THEME_CLASSES);
+  addNodeClassNames(appNode, `asi-theme-${theme}`);
+}
+
 document.body.addEventListener('mouseover', event => {
-    if (event.target.matches(TILE_SELECTOR)) {
-        updateKeywords();
-    }
+  if (event.target.matches(TILE_SELECTOR)) {
+      updateKeywords();
+  }
 }, false);
+
+document.body.addEventListener('click', ({target}) => {
+  if (target.matches(THEME_TOGGLE_SELECTOR)) {
+    const theme = target.getAttribute('data-theme');
+
+    setTheme(theme);
+
+    localStorageSet('theme', theme);
+  }
+});
+
+(function init () {
+  [...document.querySelectorAll(KEYWORD_SELECTOR)]
+    .forEach(keywordNode => {
+      removeNodeClassNames(keywordNode, ...KEYWORD_COLOR_CLASSES);
+      addNodeClassNames(keywordNode, sample(KEYWORD_COLOR_CLASSES));
+    });
+
+  const storedTheme = localStorageGet('theme');
+
+  if (storedTheme) {
+    setTheme(storedTheme);
+  }
+})();
